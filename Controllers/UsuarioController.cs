2fa3f429fef1e9cec5b79762ecb0c957bac3df6c
Javi-zafiro <javialexiscortez2@gmail.com>
2023-11-20 21:18:00 -0300

    octavo commit: se completo el controller usuario

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -24,4 +24,33 @@ public class UsuarioController : ControllerBase
         usuarioRepository.CrearUsuario(usu);
         return Ok(usu);
     }
+
+    [HttpGet("usuario")]
+    public ActionResult<List<usuario>> ListarUsuarios()
+    {
+        var lista = new List<usuario>();
+        lista= usuarioRepository.ListarUsuarios();
+        return Ok(lista);
+    }
+
+    [HttpGet("usuarioGet")]
+    public ActionResult<usuario> ObtenerUsuario(int idUsuario)
+    {
+        var user = new usuario();
+        user = usuarioRepository.ObtenerUsuario(idUsuario);
+        return Ok(user);
+    }
+
+    [HttpPost("usuarioPost/nombre")]
+    public ActionResult<usuario> CambiarNombre(int idUsuario, usuario usu)
+    {
+        if (usu==null)
+        {
+            return BadRequest("el objeto usuario es nulo");
+        }
+        usuarioRepository.ModificarUsuario(idUsuario, usu);
+        var user = new usuario();
+        user = usuarioRepository.ObtenerUsuario(idUsuario);
+        return Ok(user);
+    }
 }
